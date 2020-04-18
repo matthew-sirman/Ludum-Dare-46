cbd9c0ceffe07f3689c72019e7555e7b1423c614
mirrorworld <mirrorworld71@gmail.com>
2020-04-18 21:44:27 +0100

    money text and add money for enemy kill

--- Assets/Scripts/Environment/MoneyManager.cs
+++ Assets/Scripts/Environment/MoneyManager.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoneyManager : MonoBehaviour
 {
 
     public int money;
+    private Text moneyText;
+
+    private Dictionary<EnemyType, int> enemyKillRewards = new Dictionary<EnemyType, int>(){
+        {EnemyType.basic, 5}
+    };
+
+    void Start() {
+        moneyText = PlayerUIController.instance.moneyText;
+        Debug.Log(moneyText);
+    }
 
     public int getMoney() 
     {
         return money;
     }
 
-    public void addMoney(int amount) 
-    {
-        money += amount;
+    public void enemyKilled(EnemyType type) {
+        money += enemyKillRewards[type];
+        moneyText.text = "£" + money.ToString();
     }
 
     public void subtractMoney(int amount) 
     {
         money -= amount;
+        moneyText.text = "£" + money.ToString();
     }
 }

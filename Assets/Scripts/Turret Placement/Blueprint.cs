3427d4cab6033038df3955d10d474a6c0733cf49
WilliamH-D <61289152+WilliamH-D@users.noreply.github.com>
2020-04-18 17:43:49 +0100

    Turret stuff again
    fsdasg

--- Assets/Scripts/Turret Placement/Blueprint.cs
+++ Assets/Scripts/Turret Placement/Blueprint.cs
@@ -9,7 +9,9 @@ public class Blueprint : MonoBehaviour
     public GameObject turret;
     public int button;
 
-    public RadialMenuController controller;
+    RadialMenuController controller;
+
+    public Image img;
 
     public void Start()
     {
@@ -18,27 +20,28 @@ public class Blueprint : MonoBehaviour
 
     private void OnMouseDown()
     {
+        //Set one of the buttons on the radial menu to the weapon in the blueprint
         if (button == 1) {
             controller.setTurret1(turret);
-            controller.getButton1().GetComponentInChildren<Image>().sprite = turret.GetComponent<TurretData>().getSprite();
+            controller.getButton1().GetComponentsInChildren<Image>()[1].sprite = turret.GetComponent<TurretData>().getSprite();
             controller.getButton1().GetComponentInChildren<Text>().text = turret.GetComponentInChildren<TurretData>().getCost().ToString();
         }
         if (button == 2)
         {
             controller.setTurret2(turret);
-            controller.getButton2().GetComponentInChildren<Image>().sprite = turret.GetComponent<TurretData>().getSprite();
+            controller.getButton2().GetComponentsInChildren<Image>()[1].sprite = turret.GetComponent<TurretData>().getSprite();
             controller.getButton2().GetComponentInChildren<Text>().text = turret.GetComponentInChildren<TurretData>().getCost().ToString();
         }
         if (button == 3)
         {
             controller.setTurret3(turret);
-            controller.getButton3().GetComponentInChildren<Image>().sprite = turret.GetComponent<TurretData>().getSprite();
+            controller.getButton3().GetComponentsInChildren<Image>()[1].sprite = turret.GetComponent<TurretData>().getSprite();
             controller.getButton3().GetComponentInChildren<Text>().text = turret.GetComponentInChildren<TurretData>().getCost().ToString();
         }
         if (button == 4)
         {
             controller.setTurret4(turret);
-            controller.getButton4().GetComponentInChildren<Image>().sprite = turret.GetComponent<TurretData>().getSprite();
+            controller.getButton4().GetComponentsInChildren<Image>()[1].sprite = turret.GetComponent<TurretData>().getSprite();
             controller.getButton4().GetComponentInChildren<Text>().text = turret.GetComponentInChildren<TurretData>().getCost().ToString();
         }
         Debug.Log("YOU HAVE UNLOCKED TURRET " + turret.name);

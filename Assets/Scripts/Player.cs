82273b66ab62feaf917b6f1adeacda0e532832e2
matthew-sirman <matthew.sirman1@gmail.com>
2020-04-18 01:15:28 +0100

    Updated player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // changes made to this
     }
 
     // Update is called once per frame

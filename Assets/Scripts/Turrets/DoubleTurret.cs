62f631b2910628a422a14d533ba2a212b215624d
WilliamH-D <61289152+WilliamH-D@users.noreply.github.com>
2020-04-18 22:13:23 +0100

    turret aiming

---
+++ Assets/Scripts/Turrets/DoubleTurret.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleTurret : BaseTurret
+{
+    public string headName = "TurretGuns";
+    public int cooldownTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+       setTurret(gameObject.transform.Find(headName).gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

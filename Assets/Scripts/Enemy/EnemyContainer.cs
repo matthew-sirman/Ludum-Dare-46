3ccc57c560dc6d7957c3b31673b6be5921b14979
mirrorworld <mirrorworld71@gmail.com>
2020-04-18 22:47:28 +0100

    merge

--- Assets/Scripts/Enemy/EnemyContainer.cs
+++ Assets/Scripts/Enemy/EnemyContainer.cs
@@ -8,17 +8,20 @@ public class EnemyContainer : MonoBehaviour {
     private Vector3 spawnPos;
     private Queue<EnemyType> spawnQueue;
     public GameObject enemyPrefab;
+    private WaveManager waveManager;
 
     private float spawnInterval;
     private float lastSpawnTime;
     private bool spawnWave;
+    private int nActiveEnemies;
 
     void Start() {
+        waveManager = GameObject.FindWithTag("WaveManager").GetComponent<WaveManager>();
+        nActiveEnemies = 0;
         spawnInterval = 2.0f;
         lastSpawnTime = -spawnInterval;
         spawnWave = true;
         spawnPos = new Vector3(0, 0, 50);
-
         target = GameObject.FindWithTag("Target");
         enemies = new List<GameObject>();
         spawnQueue = new Queue<EnemyType>();
@@ -32,10 +35,15 @@ public class EnemyContainer : MonoBehaviour {
         enemy.transform.SetParent(transform);
         enemy.GetComponent<Enemy>().init(this, type);
         enemies.Add(enemy);
+        nActiveEnemies++;
     }
 
     public void removeEnemy(GameObject enemy) {
         enemies.Remove(enemy);
+        nActiveEnemies--;
+        if (nActiveEnemies == 0) {
+
+        }
     }
 
     void spawnEnemiesInQueue() {
---
+++ Assets/Scripts/Enemy/WaveManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+    private List<Dictionary<EnemyType, int>> waveComposition = new List<Dictionary<EnemyType, int>>() {
+        new Dictionary<EnemyType, int>() {
+            {EnemyType.basic, 5}
+        },
+        new Dictionary<EnemyType, int>() {
+            {EnemyType.basic, 10}
+        },
+        new Dictionary<EnemyType, int>() {
+            {EnemyType.basic, 15}
+        },
+    };
+    private List<EnemyContainer> spawners;
+    public float timeBetweenWaves;
+
+    void Start() {
+        spawners = new List<EnemyContainer>();
+        foreach (Transform child in transform) {
+            spawners.Add(
+                child.gameObject.GetComponent<EnemyContainer>());
+        }
+    }
+
+    void Update() {
+        
+    }
+}

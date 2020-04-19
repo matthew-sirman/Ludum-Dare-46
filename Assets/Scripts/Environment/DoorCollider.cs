3ad66806b48bf5253f02c7364ca5a851359a9d03
WilliamH-D <61289152+WilliamH-D@users.noreply.github.com>
2020-04-19 17:31:03 +0100

    outline (Maybe?)

--- Assets/Scripts/Environment/DoorCollider.cs
+++ Assets/Scripts/Environment/DoorCollider.cs
@@ -7,12 +7,16 @@ public class DoorCollider : MonoBehaviour
 
     Doors parent;
     public PlayerUIController cont;
+    public Material mouseOn;
+    public Material mouseOff;
+    MeshRenderer rend;
 
     private void Start()
     {
         parent = gameObject.GetComponentInParent<Doors>();
         cont = FindObjectOfType<PlayerUIController>();
         cont.doorPrice.enabled = false;
+        rend = gameObject.GetComponent<MeshRenderer>(); 
     }
     private void OnMouseOver()
     {
@@ -25,11 +29,13 @@ public class DoorCollider : MonoBehaviour
             Debug.Log("SHOW PRICE");
             cont.doorPrice.text = "£" + parent.getCost().ToString();
             cont.doorPrice.enabled = true;
+            rend.material = mouseOn;
         }
     }
 
     private void OnMouseExit()
     {
         cont.doorPrice.enabled = false;
+        rend.material = mouseOff;
     }
 }
